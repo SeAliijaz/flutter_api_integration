83bd685a3539d1116cc052a7bd414d259d80a268
SeAliijaz <se.aliijaz@gmail.com>
2023-07-21 03:18:22 +0500

    POST API (Login)

--- lib/Post-Method/Day-01-(Signup-Login)/Screen/login_screen.dart
+++ lib/Post-Method/Day-01-(Signup-Login)/Screen/login_screen.dart
@@ -1,10 +1,86 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_api_integration/Constants/k.dart';
+import 'package:flutter_api_integration/Post-Method/Day-01-(Signup-Login)/Widgets/custom_button.dart';
+import 'package:flutter_api_integration/Post-Method/Day-01-(Signup-Login)/Widgets/custom_textformfield.dart';
+import 'package:http/http.dart';
 
-class LogInScreen extends StatelessWidget {
+class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
 
   @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  ///TextEditingController
+  final TextEditingController? emailController = TextEditingController();
+  final TextEditingController? passwordController = TextEditingController();
+
+  ///Submit Form
+  void submitForm(String email, password) async {
+    try {
+      ///value assigned to a variable
+      const String uri = "https://reqres.in/api/login";
+
+      ///response
+      Response response = await post(Uri.parse(uri), body: {
+        "email",
+        email.toString(),
+        "password",
+        password.toString(),
+      });
+
+      print(response.body.toString());
+
+      ///statusCode
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body.toString());
+        print(data);
+        AppConsts.showMessage("Logged In Successfully!");
+      } else {
+        AppConsts.showMessage("Something Issue!");
+      }
+    } catch (e) {
+      AppConsts.showMessage(e.toString());
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ///Email Field
+            CustomTextFormField(
+              controller: emailController!,
+              prefixIcon: Icons.email_outlined,
+              hintText: "Enter Your Email",
+            ),
+
+            ///Password Field
+            CustomTextFormField(
+              controller: passwordController!,
+              prefixIcon: Icons.lock_outline,
+              hintText: "Enter Your Password",
+            ),
+
+            ///Action Button
+            CustomButton(
+              title: "LogIn",
+              onPressed: () {
+                ///submit form method called
+                submitForm(
+                  emailController!.text.toString(),
+                  passwordController!.text.toString(),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
